6f30c3cb2e9dfeccd1db07f29505a25c7346ad9d
Rob <fuscorob@gmail.com>
2015-10-02 16:03:40 -0700

    Comments, last test to get 100% method coverage

--- src/test/java/com/example/InMemoryQueueTest.java
+++ src/test/java/com/example/InMemoryQueueTest.java
@@ -1,10 +1,20 @@
 package com.example;
 
+import org.junit.Test;
+
 import java.util.Timer;
+
+import static org.junit.Assert.assertEquals;
 
 public class InMemoryQueueTest extends AbstractQueueTest {
     @Override
     protected QueueService createFreshQueue() {
         return new InMemoryQueueService(new Utils(new UniqueIdentifierGenerator()), new Timer());
+    }
+
+    @Test
+    public void testGetUID() {
+        assertEquals("Verifying that getUID() always returns null since it is not applicable for this implementation",
+                null, queue.getUID());
     }
 }
--- src/test/java/com/example/SqsQueueTest.java
+++ src/test/java/com/example/SqsQueueTest.java
@@ -8,6 +8,17 @@ import com.google.common.collect.ImmutableMap;
 
 import java.util.Map;
 
+/**
+ * These are actually functional tests instead of unit tests. These are not intended to run as part of continuous
+ * integration since they are not guaranteed to pass because of the dependency on the outside service, SQS.
+ *
+ * What these are valuable for is sporadic or nightly runs to check if SQS service starts functioning differently and
+ * we would need to adjust our implementations of the other QueueServices.
+ *
+ * This test class is also valuable to develop all the tests in the abstract test classes. The idea is to get the
+ * abstract tests classes working with the SQS tests first. Then we can do test driven development on the other QueueServices
+ * and be confident that they are functioning the same as SQS.
+ */
 public class SqsQueueTest extends AbstractMultiThreadQueueTest {
     @Override
     protected QueueService createFreshQueue() {
